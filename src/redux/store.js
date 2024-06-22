1b05b0eb92bd63de581e7cd59145aa3f15ae9f16
CastelliLuca <58586845+CastelliLuca@users.noreply.github.com>
2024-06-22 13:00:23 +0200

    Adding persistor (not working for now)

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,26 @@ import generalReducer from './generalSlice'
 import userReducer from './userSlice'
 import groupReducer from './groupSlice'
 import ideaReducer from './ideaSlice'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
-export default configureStore({
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedGeneraldReducer = persistReducer(persistConfig, generalReducer)
+const persistedUserdReducer = persistReducer(persistConfig, userReducer)
+const persistedGroupReducer = persistReducer(persistConfig, groupReducer)
+const persistedIdeaReducer = persistReducer(persistConfig, ideaReducer)
+
+export const store = configureStore({
   reducer: {
-    general: generalReducer,
-    user: userReducer,
-    group: groupReducer,
-    idea: ideaReducer,
+    general: persistedGeneraldReducer,
+    user: persistedUserdReducer,
+    group: persistedGroupReducer,
+    idea: persistedIdeaReducer,
   },
 })
+
+export const persistor = persistStore(store)
