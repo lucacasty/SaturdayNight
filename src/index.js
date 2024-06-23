7f2155fb8edf1d40577260f7e7934a1b7ada2803
CastelliLuca <58586845+CastelliLuca@users.noreply.github.com>
2024-06-23 13:57:14 +0200

    adding firebase

--- src/index.js
+++ src/index.js
@@ -6,6 +6,30 @@ import reportWebVitals from './reportWebVitals';
 import store from './redux/store'
 import { Provider } from 'react-redux'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCTSiEnNIlShUtptU0vPCwqUC294h65ehU",
+  authDomain: "saturdaynight-3ae1f.firebaseapp.com",
+  projectId: "saturdaynight-3ae1f",
+  storageBucket: "saturdaynight-3ae1f.appspot.com",
+  messagingSenderId: "630422418581",
+  appId: "1:630422418581:web:a12919be43aa62f9cd979c",
+  measurementId: "G-QERDYD5VG0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
