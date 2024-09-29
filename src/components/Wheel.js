2c6b506bb3f9d5c58a6d0afde11d534dbcd238c4
CastelliLuca <58586845+CastelliLuca@users.noreply.github.com>
2024-09-29 22:51:44 +0200

    Founded a fucking wheel that is working

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -1,8 +1,23 @@
-const Wheel = () => {
-    return (
-      <>
-      </>
-    );
-  }
-  
-  export default Wheel;
+import React from 'react';
+import { Wheel } from 'wheel-of-react';
+
+export default function () {
+
+  return(
+    <>
+      <Wheel
+        wheelData={
+          [
+            {color: 'black', text: "Test 1"},
+            {color: 'white', text: "Test 2"},
+            {color: 'green', text: "Test 3"},
+            {color: 'blue', text: "Test 4"},
+            {color: 'red', text: "Test 5"},
+            {color: 'yellow', text: "Test 6"}
+          ]
+        }
+        fontFamily='Roboto'
+      />
+    </>
+  )
+}
