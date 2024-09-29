5e773c74da180688a919895dc5a62b2d5019b798
CastelliLuca <58586845+CastelliLuca@users.noreply.github.com>
2024-09-30 00:04:02 +0200

    Using new wheel

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -1,23 +1,58 @@
 import React from 'react';
-import { Wheel } from 'wheel-of-react';
+import WheelComponent from './WheelComponent';
 
 export default function () {
 
+  let objIndex = {
+    "Iphone13promax": 1, 
+    "Bosesurroundspeakers": 2, 
+    "Samsung65-InchCrystalUHD4KFlatSmartTV": 3, 
+    "MacBookAirMGN6314”Display,AppleM1ChipWith8-Core": 4, 
+    "KIATELLURIDE2022": 5,
+    "SAMSUNGFRONTLOADWASHINGMACHINE16KG": 6,
+    "10GRAMSGOLDCOIN": 7,
+  }
+  const segments = [
+    "Ale",
+    "Gullo",  
+    "Luca ",
+    "Luisa",
+    "Andre",
+    "Dutto",
+    "Abra",
+  ];
+
+  const weelColors = () => {
+    let arr = [];
+    let colors = ["#EE4040", "#F0CF50", "#815CD1", "#3DA5E0", "#34A24F"];
+    segments.forEach((el) => {
+      let color = colors.shift();
+      arr.push(color);
+      colors.push(color);
+    });
+
+    return arr;
+  };
+  const segColors = weelColors();
+
+  const onFinished = (winner) => {
+    console.log(winner);
+  };
+
   return(
     <>
-      <Wheel
-        wheelData={
-          [
-            {color: 'black', text: "Test 1"},
-            {color: 'white', text: "Test 2"},
-            {color: 'green', text: "Test 3"},
-            {color: 'blue', text: "Test 4"},
-            {color: 'red', text: "Test 5"},
-            {color: 'yellow', text: "Test 6"}
-          ]
-        }
-        fontFamily='Roboto'
-      />
+      <div id="wheel-container">
+        <WheelComponent
+          segments={segments}
+          segColors={segColors}
+          winningSegment={"8"}
+          onFinished={(winner) => onFinished(winner)}
+          primaryColor="gray"
+          contrastColor="white"
+          buttonText="Spin"
+          isOnlyOnce={true}
+        />
+      </div>
     </>
   )
 }
