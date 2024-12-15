3307e1834c5e22eedf19577821f5aefa9e76e269
CastelliLuca <58586845+CastelliLuca@users.noreply.github.com>
2024-12-15 22:36:53 +0100

    Cleaning

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import HomePage from './pages/HomePage';
-import BottomNavigator from './components/BottomNavigator';
-import { useSelector, useDispatch } from 'react-redux';
 import Login from './login/Login';
 import Register from './login/Register';
 import Reset from './login/Reset';
 import Pages from './pages/Pages';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import React, { useEffect } from 'react';
+import React from 'react';
 
 function App() {
   return (
